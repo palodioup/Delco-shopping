0f76d3cc00f77fa5bffef1ca4cd3a03a2bd5bc77
Oladipupo <dipoakanji57@gmail.com>
2025-09-16 18:00:56 +0100

    More

--- src/data/Product.jsx
+++ src/data/Product.jsx
@@ -3,6 +3,7 @@ import { CartContext } from "../code/ContaxtProvider";
 
 const Product = ({ product }) => {
   const { dispatch } = useContext(CartContext);
+  const hasDiscount = product.discount
 
   return (
     <div className="w-75 sm:w-100 md:w-100 lg:w-100 xl:w-100">
@@ -10,6 +11,7 @@ const Product = ({ product }) => {
         <img src={product.thumbnail} alt="" />
         <h1>Category: {product.category}</h1>
         <h1>{product.title}</h1>
+        {hasDiscount ? <h3 className="font-black text-lg text-blue-800">Discount: {product.discount}%</h3> : <p>Discount: No discount available</p>}
         <p>${product.price}</p>
         <p>{product.description}</p>
         <button
